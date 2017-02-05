69cebf7844c7630147940ee6654746e3cc3830a5
segoviam <msegovia@mac.com>
2017-02-04 20:54:24 -0500

    added microposts in laptop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ApplicationRecord
+	has_many          :microposts, dependent: :destroy
 	attr_accessor     :remember_token, :activation_token, :reset_token
 	before_save       :downcase_email
 	before_create     :create_activation_digest
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: {maximum: 255},
-										format: {with: VALID_EMAIL_REGEX},
-										uniqueness: { case_sensitive: false }
+			  format: {with: VALID_EMAIL_REGEX},
+			  uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 # returns the hash digest of the given string.
 	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-																									BCrypt::Engine.cost
+		cost = ActiveModel::SecurePassword.min_cost ?
+		BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 		BCrypt::Password.create(string, cost: cost)																							
 	end
 
