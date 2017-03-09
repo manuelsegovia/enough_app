1d35c4d6215324ff6958b4988a8f2dd7b3264587
manuelsegovia <msegovia@mac.com>
2017-03-08 19:42:39 -0500

    eliminated s3 keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJJAQWOPX7JGULTYA'],
-      :aws_secret_access_key => ENV['WpkdYtwO/']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['segoviaimg']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
